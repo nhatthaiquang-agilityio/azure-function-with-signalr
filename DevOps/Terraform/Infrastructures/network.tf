1cde453e17161cbe3b3540904676cf0164a3a9c3
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2025-11-25 10:59:31 +0700

    Update range address

--- DevOps/Terraform/Infrastructures/network.tf
+++ DevOps/Terraform/Infrastructures/network.tf
@@ -52,7 +52,7 @@ resource "azurerm_subnet" "az_func_subnet_int" {
   name                 = "subnet-int-az-func"
   resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.az_signalr_network.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.0.3.0/24"]
 
   delegation {
     name = "example-delegation"
