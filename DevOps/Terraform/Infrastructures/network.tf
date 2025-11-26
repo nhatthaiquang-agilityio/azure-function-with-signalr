1b6320bb36436bb3187671cb8b809fe5fcc277e2
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2025-11-26 10:27:17 +0700

    Update network

--- DevOps/Terraform/Infrastructures/network.tf
+++ DevOps/Terraform/Infrastructures/network.tf
@@ -13,46 +13,25 @@ resource "azurerm_virtual_network" "az_signalr_network" {
   resource_group_name = data.azurerm_resource_group.rg.name
 }
 
-# Subnet 1: internal
-resource "azurerm_subnet" "az_signalr_subnet_int" {
-  name                 = "subnet-int-az-signalr"
-  resource_group_name  = data.azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.az_signalr_network.name
-  address_prefixes     = ["10.0.1.0/24"]
-
-  delegation {
-    name = "example-delegation"
-
-    service_delegation {
-      name    = "Microsoft.Web/serverFarms"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-    }
-  }
-  service_endpoints = [
-    "Microsoft.EventHub",
-    "Microsoft.KeyVault",
-    "Microsoft.ServiceBus",
-    "Microsoft.Storage",
-    "Microsoft.Web"
-  ]
-}
-
-# Subnet 2: private Endpoint for SignalR
+# Subnet: private Endpoint for SignalR
 resource "azurerm_subnet" "az_signalr_subnet_pe" {
   name                 = "subnet-pe-az-signalr"
   resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.az_signalr_network.name
   address_prefixes     = ["10.0.2.0/24"]
+
+  private_link_service_network_policies_enabled = true
 }
 
-
 # Azure Function
-# Subnet 1: internal
+# Subnet: internal
 resource "azurerm_subnet" "az_func_subnet_int" {
   name                 = "subnet-int-az-func"
   resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.az_signalr_network.name
   address_prefixes     = ["10.0.3.0/24"]
+
+  private_link_service_network_policies_enabled = true
 
   delegation {
     name = "example-delegation"
